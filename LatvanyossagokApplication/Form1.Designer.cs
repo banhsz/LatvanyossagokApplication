efa712c4c1e29b97a66dec74c98122d7f5386adf
Szabolcs Bánhidi <banhidi.szabolcs@petrik.hu>
2020-12-05 11:21:49 +0100

    3. a. Város hozzáadása

--- LatvanyossagokApplication/Form1.Designer.cs
+++ LatvanyossagokApplication/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LatvanyossagokApplication
 {
-    partial class Form1
+    partial class formNevezetesseg
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxVarosNeve = new System.Windows.Forms.TextBox();
+            this.numericUpDownLakossag = new System.Windows.Forms.NumericUpDown();
+            this.buttonVarosFelvetele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLakossag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxVarosNeve
+            // 
+            this.textBoxVarosNeve.Location = new System.Drawing.Point(12, 12);
+            this.textBoxVarosNeve.Name = "textBoxVarosNeve";
+            this.textBoxVarosNeve.Size = new System.Drawing.Size(162, 20);
+            this.textBoxVarosNeve.TabIndex = 0;
+            // 
+            // numericUpDownLakossag
+            // 
+            this.numericUpDownLakossag.Location = new System.Drawing.Point(12, 39);
+            this.numericUpDownLakossag.Maximum = new decimal(new int[] {
+            40000000,
+            0,
+            0,
+            0});
+            this.numericUpDownLakossag.Name = "numericUpDownLakossag";
+            this.numericUpDownLakossag.Size = new System.Drawing.Size(162, 20);
+            this.numericUpDownLakossag.TabIndex = 1;
+            // 
+            // buttonVarosFelvetele
+            // 
+            this.buttonVarosFelvetele.Location = new System.Drawing.Point(12, 66);
+            this.buttonVarosFelvetele.Name = "buttonVarosFelvetele";
+            this.buttonVarosFelvetele.Size = new System.Drawing.Size(162, 23);
+            this.buttonVarosFelvetele.TabIndex = 2;
+            this.buttonVarosFelvetele.Text = "Város felvétele";
+            this.buttonVarosFelvetele.UseVisualStyleBackColor = true;
+            this.buttonVarosFelvetele.Click += new System.EventHandler(this.buttonVarosFelvetele_Click);
+            // 
+            // formNevezetesseg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonVarosFelvetele);
+            this.Controls.Add(this.numericUpDownLakossag);
+            this.Controls.Add(this.textBoxVarosNeve);
+            this.Name = "formNevezetesseg";
+            this.Text = "Nevezetességek";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLakossag)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxVarosNeve;
+        private System.Windows.Forms.NumericUpDown numericUpDownLakossag;
+        private System.Windows.Forms.Button buttonVarosFelvetele;
     }
 }
 
--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -8,13 +8,62 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace LatvanyossagokApplication
 {
-    public partial class Form1 : Form
+    public partial class formNevezetesseg : Form
     {
-        public Form1()
+        MySqlConnection conn;
+        public formNevezetesseg()
         {
             InitializeComponent();
+            conn = new MySqlConnection("server = localhost; database = latvanyossagokdb; uid = root; pwd = ;");
+            conn.Open();
+
+            this.FormClosed += (sender, args) =>
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            };
+        }
+        void AdatBeszuras(String varosNeve,int lakossag)
+        {
+
+            string sql = @"
+            INSERT INTO varosok(nev,lakossag)
+            VALUES (@nev,@lakossag)
+            ";
+            //INSERT INTO `varosok`(`id`, `nev`, `lakossag`) VALUES ([value-1],[value-2],[value-3])
+            var comm = this.conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.Parameters.AddWithValue("@nev", varosNeve);
+            comm.Parameters.AddWithValue("@lakossag", lakossag);
+
+            try
+            {
+                comm.ExecuteNonQuery();
+                MessageBox.Show(String.Format("Sikeres adatbevitel!\n{0}\n{1}",varosNeve,lakossag),"Sikeres művelet");
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException e)
+            {
+                MessageBox.Show("Ilyen város már létezik az adatbázisban!","Hiba");
+            }
+        }
+        private void buttonVarosFelvetele_Click(object sender, EventArgs e)
+        {
+            if (textBoxVarosNeve.Text=="" || !(numericUpDownLakossag.Value > 0))
+            {
+                MessageBox.Show("Érvényes adatokat adj meg!", "Hiba");
+            }
+            else
+            {
+                AdatBeszuras(textBoxVarosNeve.Text,Convert.ToInt32(numericUpDownLakossag.Value));
+            }
         }
     }
 }
